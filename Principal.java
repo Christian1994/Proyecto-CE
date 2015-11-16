4e0f647a52c2e97cb49cc094b24f037c6259b16b
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-11-16 14:16:05 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+	
+	public static void main(String[] args) 
+	{
+		Plantel univalleTeam = new Plantel();
+		univalleTeam.mostrarPlantel();
+		
+		// AlgoritmoGenetico ga = new AlgoritmoGenetico();
+		// ga.printCromosoma();
+	}
+	
+}
