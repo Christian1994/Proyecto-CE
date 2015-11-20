aedf559ec8eab5f4a2e3ce1af370b365b262afdf
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-11-19 19:07:00 -0500

    Selección de cromosomas por torneo

--- AlgortimoGenetico.java
+++ AlgortimoGenetico.java
@@ -14,7 +14,7 @@ public class AlgoritmoGenetico {
 	// Método que genera la población de cromosomas con su aptitud. 100 cromosomas se generarán en este problema
 	public void generarPoblacion(int tam)
 	{
-		System.out.println("\nPoblación: \n");
+		System.out.println("\nPoblación inicial: \n");
 		System.out.println("Nº\tCromosoma\t\tAdaptación");
 		for(int i = 0; i < tam; i++)
 		{
@@ -26,11 +26,11 @@ public class AlgoritmoGenetico {
 	}
 		
 	// Método que realiza la selección por torneo. 
-	// Seleccionamos dos cromosomas al azar. 
-	// ¿Seleccionar el cromosoma más de una vez? O ¿sólamente una vez?.
+	// Seleccionamos dos cromosomas al azar hasta llenar el mating pool.
+	// Tamaño del mating pool: 60 cromosomas
 	public void seleccion()
 	{
-		for(int i = 0; i < (poblacion.size()/2); i++)
+		for(int i = 0; i < 60; i++)
 		{
 			// Selección de primer cromosoma de forma aleatoria
 			int i1 = (int)Math.floor(poblacion.size() * Math.random());
@@ -39,10 +39,6 @@ public class AlgoritmoGenetico {
 			// Selección de segundo cromosoma de forma aleatoria
 			int i2 = (int)Math.floor(poblacion.size() * Math.random());
 			Cromosoma crom2 = poblacion.get(i2);
-			
-			// Seleccionar y reemplazar? o sólo seleccionar?
-			// poblacion.remove(i1);
-			// poblacion.remove(i2);
 			
 			// El que tenga la mejor aptitud, éste se enviará al mating pool
 			if(crom1.getAdaptacion() >= crom2.getAdaptacion())
@@ -55,16 +51,21 @@ public class AlgoritmoGenetico {
 				matingPool.add(crom2);
 			}
 			
-			System.out.print("\n\nÍndice 1: " + (i1 + 1) + " " + crom1.getAdaptacion());
+			/* System.out.print("\n\nÍndice 1: " + (i1 + 1) + " " + crom1.getAdaptacion());
 			System.out.print("\nÍndice 2: " + (i2 + 1) + " " + crom2.getAdaptacion());
-			System.out.print("\n\nTamaño del mating pool: " + matingPool.size());
+			System.out.print("\n\nTamaño del mating pool: " + matingPool.size());*/
 		}
 		
 		// Imprime todos los cromosomas enviados al mating pool
+		System.out.println("\n\nMating pool. Selección por torneo");
+		System.out.println("\nNº\tCromosoma\tAdaptación");
 		for(int i = 0; i < matingPool.size(); i++)
 		{
 			matingPool.get(i).printCromosoma(i);
+			System.out.print("\t\t" + matingPool.get(i).getAdaptacion());
 		}
+		
+		System.out.print("\n\nTamaño del mating pool: " + matingPool.size());
 	}
 	
 	public void cruce()
