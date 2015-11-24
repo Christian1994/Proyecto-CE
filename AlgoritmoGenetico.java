33a04c9f5d7c2bd9b5c1336216a72ef8a10de834
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-11-23 21:14:12 -0500

    Pequeños cambios al operador de cruce uniforme

--- AlgoritmoGenetico.java
+++ AlgoritmoGenetico.java
@@ -78,7 +78,7 @@ public class AlgoritmoGenetico {
 	public void cruce()
 	{
 		Cromosoma cruzado = new Cromosoma();
-		ArrayList<Jugador> nuevaFormacion = cruzado.getJugadores();		// Nuevo cromosoma cruzado
+		ArrayList<Jugador> nuevaFormacion = cruzado.getJugadores();			// Nuevo cromosoma cruzado
 		ArrayList<Jugador> team = new ArrayList<Jugador>();					// Cromosoma filtro para el cruzamiento
 		ArrayList<Jugador> aux = new ArrayList<Jugador>();					// Cromosoma auxiliar para asegurar la no repetición de genes
 		ArrayList<Cromosoma> parejaCromosomas = new ArrayList<Cromosoma>();	// Parejas de cromosomas seleccionada para su reproducción
@@ -139,93 +139,6 @@ public class AlgoritmoGenetico {
 				System.out.print(cruzado.getJugadores().get(i).getNumero() + " ");
 			}			
 		}
-	}
-	
-	public void mutacion()
-	{
-		
-	}
-	
-	public void reemplazo()
-	{
-		
-	}
-}
-import java.util.ArrayList;
-
-public class AlgoritmoGenetico {
-	
-	private ArrayList<Cromosoma> poblacion = new ArrayList<Cromosoma>();	// PoblaciÃ³n de cromosomas (Opciones de formaciÃ³n titular)
-	private ArrayList<Cromosoma> matingPool = new ArrayList<Cromosoma>();	// Mating pool
-	
-	// Constructor
-	public AlgoritmoGenetico()
-	{
-
-	}
-	
-	// Método que genera la población de cromosomas con su aptitud. 100 cromosomas se generarán en este problema
-	public void generarPoblacion(int tam)
-	{
-		System.out.println("\nPoblación inicial: \n");
-		System.out.println("Nº\tCromosoma\t\tAdaptación");
-		for(int i = 0; i < tam; i++)
-		{
-			System.out.print((i + 1) + "\t");
-			poblacion.add(new Cromosoma());
-			System.out.print("\t\t" + poblacion.get(i).getAdaptacion() + "\n");
-		}
-		
-		System.out.print("\n\nTamaño de población: " + poblacion.size());
-	}
-		
-	// Método que realiza la selección por torneo. 
-	// Seleccionamos dos cromosomas al azar hasta llenar el mating pool.
-	// Tamaño del mating pool: 60 cromosomas
-	public void seleccion()
-	{
-		for(int i = 0; i < 60; i++)
-		{
-			// Selección de primer cromosoma de forma aleatoria
-			int i1 = (int)Math.floor(poblacion.size() * Math.random());
-			Cromosoma crom1 = poblacion.get(i1);
-			
-			// Selección de segundo cromosoma de forma aleatoria
-			int i2 = (int)Math.floor(poblacion.size() * Math.random());
-			Cromosoma crom2 = poblacion.get(i2);
-			
-			// El que tenga la mejor aptitud, éste se enviará¡ al mating pool
-			if(crom1.getAdaptacion() >= crom2.getAdaptacion())
-			{
-				matingPool.add(crom1);
-			}
-			
-			else if(crom2.getAdaptacion() >= crom1.getAdaptacion())
-			{
-				matingPool.add(crom2);
-			}
-			
-			/* System.out.print("\n\nÍndice 1: " + (i1 + 1) + " " + crom1.getAdaptacion());
-			System.out.print("\nÍndice 2: " + (i2 + 1) + " " + crom2.getAdaptacion());
-			System.out.print("\n\nTamaño del mating pool: " + matingPool.size());*/
-		}
-		
-		// Imprime todos los cromosomas enviados al mating pool
-		System.out.println("\n\nMating pool. Selección por torneo");
-		System.out.println("\nNº\tCromosoma\tAdaptación");
-		for(int i = 0; i < matingPool.size(); i++)
-		{
-			System.out.print((i + 1) + "\t");
-			matingPool.get(i).printCromosoma();
-			System.out.print("\t\t" + matingPool.get(i).getAdaptacion() + "\n");
-		}
-		
-		System.out.print("\n\nTamaño del mating pool: " + matingPool.size());
-	}
-	
-	public void cruce()
-	{
-		
 	}
 	
 	public void mutacion()
