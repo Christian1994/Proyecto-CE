8ca84693acaee9d25cc1954b48d368f96751c4bf
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-12-01 21:43:48 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -8,10 +8,10 @@ public class Principal {
 		
 		AlgoritmoGenetico ga = new AlgoritmoGenetico();
 		ga.generarPoblacion(100);		// Población de 100 individuos
-		ga.seleccion();
-		// ga.cruce();
-		// ga.mutacion();
-		// ga.reemplazo();
+		ga.seleccion(60);				// Población de 60 individuos dentro del mating pool
+		ga.reproduccion(0.5, 0.05);		// Reproducción con probabilidad de cruce del 50% y con probabilidad de mutación de 5%
+		ga.reemplazo();					// Generación de nueva población
+
 	}
 	
 }
