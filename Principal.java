18621a5094f3b8c39fea77ee0926baa16e8a7469
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-12-01 23:00:40 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -6,12 +6,15 @@ public class Principal {
 		Plantel univalleTeam = new Plantel();
 		univalleTeam.mostrarPlantel();
 		
+		// Pendiente determinar el criterio de parada del AG
+		
 		AlgoritmoGenetico ga = new AlgoritmoGenetico();
 		ga.generarPoblacion(100);		// Población de 100 individuos
-		ga.seleccion();
-		// ga.cruce();
-		// ga.mutacion();
-		// ga.reemplazo();
+		ga.seleccion(60);				// Población de 60 individuos dentro del mating pool
+		ga.reproduccion(0.5, 0.05);		// Reproducción con probabilidad de cruce del 50% y con probabilidad de mutación de 5%
+		ga.reemplazo();					// Generación de nueva población
+		ga.evaluacion();
+
 	}
 	
 }
